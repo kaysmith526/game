3abdf35f90ac1694669337a18755f21e51b68056
kaysmith526 <134226921+kaysmith526@users.noreply.github.com>
2023-05-21 14:09:42 -0400

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StarterGame.
+    
+    s
+{
+    public class Item : IItem
+    {
+        private string _name;
+        private float _weight;
+        private float _value;
+
+        public string Name { get { return _name; } }
+        public float Weight { get { return _weight; } }
+        public float Value { get { return _value; } }
+        public string Description { get { return Name + ", weight=" + Weight; } }
+
+        public Item() : this("NoName") { }
+        public Item(string name) : this(name, 1f) { }
+        public Item(string name, float weight) : this(name, weight, 1f) { }
+
+        public Item(string name, float weight, float value)
+        {
+            _name = name;
+            _weight = weight;
+            _value = value;
+        }
+    }
+}
+
---
+++ Key.cs
@@ -0,0 +1,32 @@
+﻿using StarterGame;
+using System;
+
+public class Key : IItem
+{
+    private string _name;
+    private float _weight;
+    private float _value;
+    private string _uuid;
+
+    public string Name { get { return _name; } }
+    public float Weight { get { return _weight; } }
+    public float Value { get { return _value; } }
+    public string Description { get { return Name + ", weight=" + Weight; } }
+
+    public Key() : this("UnknownKey") { }
+    public Key(string name) : this(name, 1f) { }
+    public Key(string name, float weight) : this(name, weight, 1f) { }
+
+    public Key(string name, float weight, float value)
+    {
+        _name = name;
+        _weight = weight;
+        _value = value;
+        _uuid = Guid.NewGuid().ToString(); //makes sure each key is unique using uuid
+    }
+
+    public bool Equals(Key other)
+    {
+        return _uuid == other._uuid;
+    }
+}
